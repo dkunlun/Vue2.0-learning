5424ea6f5e61faa31b8e21337f3764a0692afe7b
dkunlun <519395527@QQ.COM>
2016-11-18 23:54:58 +0800

    style fixed

--- src/vue-draggable/draggable.vue
+++ src/vue-draggable/draggable.vue
@@ -1,23 +1,28 @@
 <template>
 	<div id="app">
-		<ul>
-			<li v-for="(item, index) in list" 
-				draggable="true" 
-				@drop="handleDrop($event, index)" @dragover="allowDrop" 
-				@dragstart="handleDragStart($event, index)"
-				@dragend="handleDragEnd($event, index)">
-				{{item.name}}
-			</li>
-		</ul>
-		<div>
-			<ul style="width: 1000px;">
-				<li v-for="(item, index) in list" 
-					class="draggItem"
-					:class="[item.color, {'dragging': item.dragging}]" 
+		<div class="left">
+			<ul>
+				<li v-for="(item, index) in list"
+					class="listItem" 
 					draggable="true" 
 					@drop="handleDrop($event, index)" @dragover="allowDrop" 
 					@dragstart="handleDragStart($event, index)"
 					@dragend="handleDragEnd($event, index)">
+					{{item.color}}
+				</li>
+			</ul>
+		</div>
+		<div class="right">
+			<ul style="width: 400px;">
+				<li v-for="(item, index) in list" 
+					class="draggItem"
+					:class="[{'dragging': item.dragging}]"
+					:style="{background: item.color}" 
+					draggable="true" 
+					@drop="handleDrop($event, index)" @dragover="allowDrop" 
+					@dragstart="handleDragStart($event, index)"
+					@dragend="handleDragEnd($event, index)">
+				{{item.color}}
 				</li>
 			</ul>
 		</div>
@@ -31,39 +36,39 @@
 			return {
 				list: [
 					{
-						id: 'drag1',
 						color: 'red',
-						name: 'one',
 						dragging: false
 					},
 					{
-						id: 'drag2',
 						color: 'green',
-						name: 'two',
 						dragging: false
 					},
 					{
-						id: 'drag3',
 						color: 'blue',
-						name: 'three',
 						dragging: false
 					},
 					{
-						id: 'drag4',
 						color: 'orange',
-						name: 'four',
 						dragging: false
 					},
 					{
-						id: 'drag5',
 						color: 'yellow',
-						name: 'five',
 						dragging: false
 					},
 					{
-						id: 'drag6',
 						color: 'skyblue',
-						name: 'six',
+						dragging: false
+					},
+					{
+						color: 'hotpink',
+						dragging: false
+					},
+					{
+						color: 'burlywood',
+						dragging: false
+					},
+					{
+						color: 'Cornflowerblue',
 						dragging: false
 					}
 				],
@@ -97,31 +102,37 @@
 </script>
 
 <style>
+	body {
+		font-family: Helvetica,sans-serif;
+	}
+	.listItem {
+		background: #f5f5f5;
+		padding: .5rem;
+		color: #5f5f5f;
+		transition: transform .3s;
+	}
 	.draggItem{
-		width: 300px;
-		height: 200px;
+		width: 33%;
+		height: 6rem;
+		background: #efefef;
+		line-height: 6rem;
+		text-align: center;
+		color: #fff;
+		transition: transform .3s;
 		display: inline-block;
-		margin: 10px 10px;
-	}
-	.red {
-		background: red;
-	}
-	.green {
-		background: green;
-	}
-	.blue {
-		background: blue;
-	}
-	.orange {
-		background: orange;
-	}
-	.skyblue {
-		background: skyblue;
-	}
-	.yellow {
-		background: yellow;
 	}
 	.dragging {
 		transform: scale(1.1);
+	}
+	.left {
+		display: inline-block;
+	}
+	.right {
+		display: inline-block;
+	    display: -ms-flexbox;
+	    display: flex;
+	    -ms-flex-wrap: wrap;
+	    flex-wrap: wrap;
+	    width: 30rem;
 	}
 </style>
