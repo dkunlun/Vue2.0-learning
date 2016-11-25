65ccec9a37e4fbf939722cf4b92e9355268fb53f
dkunlun <519395527@qq.com>
2016-11-25 18:11:12 +0800

    add book.vue

---
+++ src/vue-books/book.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<div id="left">
+			<ul class="list">
+				<li v-for="item in shopcartList">{{item.name}}</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<style type="text/css">
+	li {
+		display: block;
+	}
+	.list li {
+		padding: 20px 20px;
+		border: 1px solid;
+	}
+	#left {
+		width: 10%;
+		height: 100%;
+	}
+</style>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+	name: 'app',
+	computed: mapGetters([
+		'shopcartList'
+	]),
+	methods: mapActions([
+		'getShopCartList'
+	]),
+	data() {
+		return {
+			msg: 'First page!'
+		}
+	},
+	created () {
+		this.getShopCartList()
+	}
+}
+</script>
