7b6bfb134d10c8a1981885aab5b981860f5a36fa
dkunlun <519395527@qq.com>
2016-11-28 19:39:48 +0800

    package.json config

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,12 @@ module.exports = {
 			options: {
 				name: '[name].[ext]?[hash]'
 			}
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+			loader: 'file-loader'
 		}]
 	},
 	resolve: {
